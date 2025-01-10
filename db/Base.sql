2d60a96c1ec6099f3ea0c8a1bee77edf8a764b42
502 Bad Gateway <kaneko.takumu@it-mirai-h.ibk.ed.jp>
2025-01-10 22:27:06 +0900

    MySQLのDBの初期化についての記載

--- db/Base.sql
+++ db/Base.sql
@@ -1,33 +1,27 @@
-CREATE DATABASE "ITmiraiApp";
+CREATE DATABASE IF NOT EXISTS `ITmiraiApp`;
 
-USE "ITmiraiApp";
+USE `ITmiraiApp`;
 
-CREATE TABLE "ChangeOfClass" (
-  "date" date NOT NULL,
-  "class" varchar(5) NOT NULL,
-  "time" varchar(10) NOT NULL,
-  "After" text NOT NULL,
-  "label" text DEFAULT NULL
+CREATE TABLE IF NOT EXISTS `ChangeOfClass` (
+  `Date` date NOT NULL,
+  `ClassID` varchar(5) NOT NULL,
+  `Time` varchar(10) NOT NULL,
+  `After` text NOT NULL,
+  `Label` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE "PhoneOAuth" (
-  "DeviceName" varchar(10) NOT NULL,
-  "Pass" text NOT NULL,
-  PRIMARY KEY ("DeviceName")
+CREATE TABLE IF NOT EXISTS `Users` (
+  `uid` int(11) NOT NULL,
+  `name` varchar(10) NOT NULL,
+  `photoURL` text NOT NULL,
+  `ClassInSchool` int(11) NOT NULL,
+  `GradeInSchool` int(11) NOT NULL,
+  `email` text NOT NULL,
+  `SchoolClub` text NOT NULL,
+  `FriendCode` int(11) NOT NULL,
+  `FriendList` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  PRIMARY KEY (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE "Users" (
-  "uid" int(11) NOT NULL,
-  "name" varchar(10) NOT NULL,
-  "photoURL" text NOT NULL,
-  "ClassInSchool" int(11) NOT NULL,
-  "GradeInSchool" int(11) NOT NULL,
-  "email" text NOT NULL,
-  "SchoolClub" text NOT NULL,
-  "FriendCode" int(11) NOT NULL,
-  "FriendList" longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  PRIMARY KEY ("uid")
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-CREATE USER 'MiraiCore'@'%' identified by 'KT34i5kirQpV';
-GRANT ALL on ITmiraiApp.* to 'MiraiCore'@'%';
+CREATE USER IF NOT EXISTS 'MiraiCore'@'%' IDENTIFIED BY 'KT34i5kirQpV';
+GRANT ALL ON `ITmiraiApp`.* TO 'MiraiCore'@'%';
